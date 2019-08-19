fd9d1c3d6d95ff98b69e36efac617c4fc7e76558
Łukasz Walocha <lwa@fedora.lwa>
2019-08-19 15:55:31 +0200

    changing Readme

---
+++ elk_download.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+mkdir -p rpms
+cd rpms
+#wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.3.0-x86_64.rpm
+#wget https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-7.3.0-x86_64.rpm
+#wget https://artifacts.elastic.co/downloads/beats/auditbeat/auditbeat-7.3.0-x86_64.rpm
+wget https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.3.0-x86_64.rpm
+wget https://artifacts.elastic.co/downloads/kibana/kibana-7.3.0-x86_64.rpm
